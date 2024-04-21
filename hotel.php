4db5e18870f588daffb1aa236423643366d59f81
Farah Arar <farah02arar@gmail.com>
2024-04-21 16:47:10 +0300

    done

---
+++ hotel.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="hotel.css" rel="stylesheet"/>
+    <script src="hotel.js" defer></script>
+    <title>WonderWise</title>
+</head>
+<body>
+    <div class="amman-img-container">
+        <img class="amman-img" src="https://www.kone.ca/fr/Images/le-royal-hero-1440x670_tcm93-11647.jpg?v=3" alt="amman img">
+        <div class="img-caption">
+            Hotels
+        </div>
+        </div>
+        <select id="cityFilter">
+            <option value="">All</option>
+            <option value="Amman">Amman</option>
+            <option value="Ajloun">Ajloun</option>
+            <option value="Irbid">Irbid</option>
+            <option value="Aqaba">Aqaba</option>
+            <option value="Petra">Petra</option>
+            <option value="Wadi Rum">Wadi Rum</option>
+            <option value="Madaba">Madaba</option>
+
+          </select>
+          
+          <div id="hotelList"></div>
+          
+        <div class="hotel-container">
+        </div>
+        
+    
+</body>
+</html>
---
+++ restaurant.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="restaurant.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <script src="restaurant .js" defer></script>
+    <title>WonderWise</title>
+</head>
+<body>
+    <div class="food-img-container">
+    <img class="food-img" src="https://romerogroup.jo/wp-content/uploads/2022/05/6-traditional-authentic-jordanian-mediterranian-breakfast-mezze-main-page-scaled.jpg" alt="food img">
+    <div class="img-caption">
+        Restaurants
+    </div>
+    </div>
+    <select id="categoryFilter">
+        <option value="">All</option>
+        <option value="Jordanian">Jordanian</option>
+        <option value="Cafe">Cafe</option>
+        <option value="Middle Eastern">Middle Eastern</option>
+        <option value="Lebanese">Mediterranean</option>
+     
+    </select>
+    <div class="restaurant-container">
+      
+    </div>  
+
+    
+</body>
+</html>
